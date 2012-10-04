46b0539d70a1ccad9810459e6714a96e7418e20c
Petter Holmstrom <petter.holmstrom@abo.fi>
2012-10-04 16:07:55 +0300

    Added PackageInfo and PackageMap classes

---
+++ i18n4vaadin-ap/src/main/java/com/github/peholmst/i18n4vaadin/ap/PackageInfo.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2012 Petter Holmström
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.peholmst.i18n4vaadin.ap;
+
+import com.github.peholmst.i18n4vaadin.annotations.BundleGeneration;
+import com.github.peholmst.i18n4vaadin.annotations.BundleStrategy;
+import com.github.peholmst.i18n4vaadin.annotations.Message;
+import com.github.peholmst.i18n4vaadin.annotations.Messages;
+import java.lang.annotation.Annotation;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import javax.lang.model.element.PackageElement;
+
+/**
+ * Class containing information about the messages declared in one specific Java
+ * package. <p><b>This class is internal and should never be used by
+ * clients.</b>
+ *
+ * @author Petter Holmström
+ */
+final class PackageInfo {
+
+    private final Map<Locale, List<Message>> messageMap = new HashMap<Locale, List<Message>>();
+    private final PackageMap owner;
+    private final PackageElement pkg;
+
+    PackageInfo(PackageMap owner, PackageElement pkg) {
+        this.owner = owner;
+        this.pkg = pkg;
+    }
+
+    String getName() {
+        return pkg.getQualifiedName().toString();
+    }
+
+    PackageMap getOwner() {
+        return owner;
+    }
+
+    PackageElement getElement() {
+        return pkg;
+    }
+
+    BundleGeneration getBundleGeneration() {
+        final BundleGeneration bundleGeneration = pkg.getAnnotation(BundleGeneration.class);
+        if (bundleGeneration == null) {
+            final PackageInfo parent = getParent();
+            if (parent == null) {
+                return new BundleGeneration() {
+                    @Override
+                    public BundleStrategy strategy() {
+                        return BundleStrategy.STANDALONE_PACKAGE_BUNDLES;
+                    }
+
+                    @Override
+                    public Class<? extends Annotation> annotationType() {
+                        return BundleGeneration.class;
+                    }
+                };
+            } else {
+                return parent.getBundleGeneration();
+            }
+        } else {
+            return bundleGeneration;
+        }
+    }
+
+    Set<Locale> getLocales() {
+        return Collections.unmodifiableSet(messageMap.keySet());
+    }
+
+    void addMessage(final Message message) {
+        final Locale locale = Utils.convertLocaleAnnotation(message.locale());
+        List<Message> messageList = messageMap.get(locale);
+        if (messageList == null) {
+            messageList = new LinkedList<Message>();
+            messageMap.put(locale, messageList);
+        }
+        messageList.add(message);
+    }
+
+    void addMessages(final Messages messages) {
+        for (final Message msg : messages.value()) {
+            addMessage(msg);
+        }
+    }
+
+    List<Message> getMessages(final Locale locale) {
+        final List<Message> messageList = messageMap.get(locale);
+        if (messageList == null) {
+            return Collections.emptyList();
+        } else {
+            return Collections.unmodifiableList(messageList);
+        }
+    }
+
+    PackageInfo getParent() {
+        String packageName = pkg.getQualifiedName().toString();
+        while (Utils.hasParentPackage(packageName)) {
+            packageName = Utils.getParentPackage(packageName);
+            final PackageInfo pkgInfo = owner.getPackageByName(packageName);
+            if (pkgInfo != null) {
+                return pkgInfo;
+            }
+        }
+        return null;
+    }
+
+    Set<PackageInfo> getSubPackages() {
+        return owner.getSubPackages(this);
+    }
+
+    boolean isRoot() {
+        return getParent() == null;
+    }
+}
---
+++ i18n4vaadin-ap/src/main/java/com/github/peholmst/i18n4vaadin/ap/PackageMap.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2012 Petter Holmström
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.peholmst.i18n4vaadin.ap;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.lang.model.element.PackageElement;
+
+/**
+ * Class containing a collection of {@link PackageInfo} objects. <p><b>This
+ * class is internal and should never be used by clients.</b>
+ *
+ * @author Petter Holmström
+ */
+final class PackageMap {
+
+    private final Map<String, PackageInfo> packages = new HashMap<String, PackageInfo>();
+
+    PackageInfo getPackage(PackageElement pkg) {
+        PackageInfo pkgInfo = packages.get(pkg.getQualifiedName().toString());
+        if (pkgInfo == null) {
+            return addPackage(pkg);
+        } else {
+            return pkgInfo;
+        }
+    }
+
+    PackageInfo addPackage(PackageElement pkg) {
+        PackageInfo pkgInfo = new PackageInfo(this, pkg);
+        packages.put(pkg.getQualifiedName().toString(), pkgInfo);
+        return pkgInfo;
+    }
+
+    PackageInfo getPackageByName(String packageName) {
+        return packages.get(packageName);
+    }
+
+    Set<PackageInfo> getRootPackages() {
+        final Set<PackageInfo> roots = new HashSet<PackageInfo>();
+        for (PackageInfo pkgInfo : packages.values()) {
+            if (pkgInfo.isRoot()) {
+                roots.add(pkgInfo);
+            }
+        }
+        return roots;
+    }
+
+    Set<PackageInfo> getSubPackages(PackageInfo parent) {
+        final Set<PackageInfo> subPackages = new HashSet<PackageInfo>();
+        for (PackageInfo pkgInfo : packages.values()) {
+            if (pkgInfo.getName().startsWith(parent.getName())) {
+                subPackages.add(pkgInfo);
+            }
+        }
+        return subPackages;
+    }
+}
